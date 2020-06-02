c4ef8ea072d0dcd282da0b891273fee3fc980d28
DevSamaa <samaakanani@gmail.com>
2020-06-02 14:55:31 +1000

    first commit

---
+++ YatzyKata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YatzyKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
