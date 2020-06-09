9faf5d1c74a4dc15699bb234254f450afc507c88
DevSamaa <samaakanani@gmail.com>
2020-06-09 20:56:15 +1000

    finished basic scoring system

--- YatzyKata/ScoreCalculator.cs
+++ YatzyKata/ScoreCalculator.cs
@@ -11,7 +11,6 @@ namespace YatzyKata
             return rolledDice.Sum();
         }
         
-        
         public int Yatzy(List<int> rolledDice)
         {
             return rolledDice.All(dice => dice == rolledDice[0])? 50:0;
@@ -51,7 +50,130 @@ namespace YatzyKata
         {
             return rolledDice.Where(dice => dice == numberToBeAdded).Sum();
         }
+
+        public int TwoPairs(List<int>rolledDice)
+        {
+            var numberToBeChecked = 6;
+            var total = 0;
+            var pairsFound = 0;
+            while (numberToBeChecked >0 && pairsFound<2)
+            {
+                var amountOfNumberFound = rolledDice.Count(dice => dice == numberToBeChecked);
+                if (amountOfNumberFound >=2)
+                {
+                   total += numberToBeChecked * 2;
+                   pairsFound++;
+                }
+                numberToBeChecked -= 1;  
+            }
+            return pairsFound<2 ? 0:total;
+        }
+        
+        public int Pairs(List<int>rolledDice)
+        {
+            return AddUpMultiples(rolledDice, 2);
+        }
+
+        public int ThreeOfAKind(List<int>rolledDice)
+        {
+            return AddUpMultiples(rolledDice, 3);
+        }
+
+        public int FourOfAKind(List<int> rolledDice)
+        {
+            return AddUpMultiples(rolledDice, 4);
+        }
+
+        private int AddUpMultiples(List<int>rolledDice,int multiple)
+        {
+            var numberToBeChecked = 6;
+            while (numberToBeChecked >0)
+            {
+                var amountOfNumberFound = rolledDice.Count(dice => dice == numberToBeChecked);
+                if (amountOfNumberFound >=multiple)
+                {
+                    return numberToBeChecked * multiple;
+                }
+                numberToBeChecked -= 1;  
+            }
+            return 0;
+        }
+
+        public int SmallStraight(List<int> rolledDice)
+        {
+            List<int> smallStraightNumbers = new List<int>{1,2,3,4,5};
+            return SumOfStraight(rolledDice, smallStraightNumbers);
+        }
+        
+        public int LargeStraight(List<int> rolledDice)
+        {
+            List<int> largeStraightNumbers = new List<int>{2,3,4,5,6};
+            return SumOfStraight(rolledDice, largeStraightNumbers);
+        }
+
+        private int SumOfStraight(List<int> rolledDice, List<int>straightNumbers)
+        {
+            rolledDice.Sort();
+            bool isEqual = rolledDice.SequenceEqual(straightNumbers);
+            if (isEqual)
+            {
+                return rolledDice.Sum();
+            }
+            return 0;
+        }
+        
+        public int FullHouse(List<int>rolledDice)
+        {
+            var numberToBeChecked = 6;
+            var total = 0;
+            var pairsFound = 0;
+            
+            while (numberToBeChecked >0)
+            {
+                var numberFound = rolledDice.Where(dice => dice == numberToBeChecked);
+                if (numberFound.Count() ==3)
+                {
+                    total += numberToBeChecked * 3;
+                    pairsFound++;
+
+                    var remainingNumbers = rolledDice.Where(dice => dice != numberToBeChecked).ToArray();
+                    if (remainingNumbers[0] == remainingNumbers[1])
+                    {
+                        total += remainingNumbers.Sum();
+                        pairsFound++;
+                    }
+                }
+                numberToBeChecked -= 1;  
+            }
+            return pairsFound<2? 0:total;
+        }
+        
+        public int FullHouse2(List<int>rolledDice)
+        {
+            var numberToBeChecked = 6;
+            var pairsFound = 0;
+            
+            while (numberToBeChecked >0)
+            {
+                var numberFound = rolledDice.Where(dice => dice == numberToBeChecked);
+                if (numberFound.Count() ==3)
+                {
+                    pairsFound++;
+
+                    var remainingNumbers = rolledDice.Where(dice => dice != numberToBeChecked).ToArray();
+                    if (remainingNumbers[0] == remainingNumbers[1])
+                    {
+                        pairsFound++;
+                    }
+                }
+                numberToBeChecked -= 1;  
+            }
+            return pairsFound<2? 0:rolledDice.Sum();
+        }
         
     }
+    
+    //TODO - ask why numberToBeChecked is yellow underlines, I didn't understand the IDE explanation/suggestion.
+    //TODO - get feedback on FullHouse/FullHouse2, which one is "better" in FullHouse2 I don't keep track of the actual total score, just add up all numbers in the end.
 }
 
--- YatzyTests/ScoreCalculatorTests.cs
+++ YatzyTests/ScoreCalculatorTests.cs
@@ -148,10 +148,141 @@ namespace YatzyTests
             var result = _scoreCalculator.Sixes(rolledDice);
             Assert.Equal(expectedOutcome, result);
         }
+        
+        // Step 9 - Pairs Category
+        public static IEnumerable<object[]> PairsCategory() 
+        {
+            yield return new object[] {new List<int>() { 3,3,3,4,4 }, 8 };
+            yield return new object[] {new List<int>() { 1,1,6,2,6 }, 12 };
+            yield return new object[] {new List<int>() { 3,3,3,4,1 }, 6 };
+            yield return new object[] {new List<int>() { 3,3,3,3,1 }, 6 };
+            yield return new object[] {new List<int>() { 6,5,5,5,4 }, 10 };
+        }
+      
+        [Theory]
+        [MemberData(nameof(PairsCategory))]
+        public void PairsShouldReturnSumOfPairs(List<int>rolledDice, int expectedOutcome)
+        {
+            var result = _scoreCalculator.Pairs(rolledDice);
+            Assert.Equal(expectedOutcome, result);
+        }
+        
+        // Step 10 - TwoPairs Category
+        public static IEnumerable<object[]> TwoPairsCategory() 
+        {
+            yield return new object[] {new List<int>() { 1,1,2,3,3 }, 8 };
+            yield return new object[] {new List<int>() { 1,1,2,3,4 }, 0 };
+            yield return new object[] {new List<int>() { 1,1,2,2,2 }, 6 };
+        }
+      
+        [Theory]
+        [MemberData(nameof(TwoPairsCategory))]
+        public void TwoPairsShouldReturnSumOfPairs(List<int>rolledDice, int expectedOutcome)
+        {
+            var result = _scoreCalculator.TwoPairs(rolledDice);
+            Assert.Equal(expectedOutcome, result);
+        }
+        
+        //Step 11 - Three Of A kind Category
+        public static IEnumerable<object[]> ThreeOfAKindCategory() 
+        {
+            yield return new object[] {new List<int>() { 3,3,3,4,5 }, 9 };
+            yield return new object[] {new List<int>() { 3,3,4,5,6 }, 0 };
+            yield return new object[] {new List<int>() { 3,3,3,3,1 }, 9 };
+            yield return new object[] {new List<int>() { 6,6,6,6,1 }, 18 };
+        }
+        [Theory]
+        [MemberData(nameof(ThreeOfAKindCategory))]
+        public void ThreeOfAKindShouldReturnSumOfThreeNumbers(List<int>rolledDice, int expectedOutcome)
+        {
+            var result = _scoreCalculator.ThreeOfAKind(rolledDice);
+            Assert.Equal(expectedOutcome, result);
+        }
+        
+        //Step 12 - Four of a kind Category
+        public static IEnumerable<object[]> FourOfAKindCategory() 
+        {
+            yield return new object[] {new List<int>() { 2,2,2,2,5 }, 8 };
+            yield return new object[] {new List<int>() { 2,2,2,5,5 }, 0 };
+            yield return new object[] {new List<int>() { 2,2,2,2,2 }, 8 };
+            yield return new object[] {new List<int>() { 6,6,6,6,1 }, 24 };
+        }
+        [Theory]
+        [MemberData(nameof(FourOfAKindCategory))]
+        public void FourOfAKindShouldReturnSumOfFourNumbers(List<int>rolledDice, int expectedOutcome)
+        {
+            var result = _scoreCalculator.FourOfAKind(rolledDice);
+            Assert.Equal(expectedOutcome, result);
+        }
+        
+        //Step 13 - Small Straight
+        public static IEnumerable<object[]> SmallStraightCategory() 
+        {
+            yield return new object[] {new List<int>() { 1,2,3,4,5 }, 15 };
+            yield return new object[] {new List<int>() { 5,3,2,4,1 }, 15 };
+            yield return new object[] {new List<int>() { 1,2,3,4,6 }, 0 };
+            yield return new object[] {new List<int>() { 2,3,4,5,6 }, 0 };
+            yield return new object[] {new List<int>() { 2,3,4,5,6 }, 0 };
+
+        }
+        
+        [Theory]
+        [MemberData(nameof(SmallStraightCategory))]
+        public void SmallStraightShouldReturn15(List<int>rolledDice, int expectedOutcome)
+        {
+            var result = _scoreCalculator.SmallStraight(rolledDice);
+            Assert.Equal(expectedOutcome, result);
+        }
+        
+        //Step 14 - Large Straight
+        public static IEnumerable<object[]> LargeStraightCategory() 
+        {
+            yield return new object[] {new List<int>() { 2,3,4,5,6 }, 20 };
+            yield return new object[] {new List<int>() { 6,4,5,2,3 }, 20 };
+            yield return new object[] {new List<int>() { 1,2,3,4,6 }, 0 };
+            yield return new object[] {new List<int>() { 1,2,3,4,5 }, 0 };
+
+        }
+        
+        [Theory]
+        [MemberData(nameof(LargeStraightCategory))]
+        public void LargeStraightShouldReturn20(List<int>rolledDice, int expectedOutcome)
+        {
+            var result = _scoreCalculator.LargeStraight(rolledDice);
+            Assert.Equal(expectedOutcome, result);
+        }
+        
+        // Step 15 - Full house
+        // Full house: If the dice are two of a kind and three of a kind,
+        // the player scores the sum of all the dice. For example, when placed on “full house”
+        // 1,1,2,2,2 scores 8 (1+1+2+2+2)
+        // 2,2,3,3,4 scores 0
+        // 4,4,4,4,4 scores 0
+        
+        public static IEnumerable<object[]> FullHouseCategory() 
+        {
+            yield return new object[] {new List<int>() { 1,1,2,2,2 }, 8 };
+            yield return new object[] {new List<int>() { 3,3,6,6,6 }, 24 };
+            yield return new object[] {new List<int>() { 2,2,3,3,4 }, 0 };
+            yield return new object[] {new List<int>() { 4,4,4,4,4 }, 0 };
+            yield return new object[] {new List<int>() { 4,4,4,6,6 }, 24 };
+            yield return new object[] {new List<int>() { 4,4,4,5,1 }, 0 };
+            yield return new object[] {new List<int>() { 1,5,1,5,1 }, 13 };
+
+
+
+
+        }
+        
+        [Theory]
+        [MemberData(nameof(FullHouseCategory))]
+        public void FullHouseShouldReturnSumOfAllDice(List<int>rolledDice, int expectedOutcome)
+        {
+            var result = _scoreCalculator.FullHouse2(rolledDice);
+            Assert.Equal(expectedOutcome, result);
+        }
     }
     
 }
-
-
 
 
